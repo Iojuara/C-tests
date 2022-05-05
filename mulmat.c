b1f636a1b5067d0eed83b8c42d131a5e1f092cbd
Inácio Araújo <inacio.neto.095@ufrn.br>
2022-05-05 20:51:36 -0300

    getting better

--- mulmat.c
+++ mulmat.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 
-  int l1, c1, l2, c2, soma = 0, k = 0;
+  int l1, c1, l2, c2;
 
   printf("Digite a primeira linhas e colunas da matriz 1:");
   scanf("%d %d", &l1, &c1);
@@ -17,13 +17,13 @@ int main(void)
 
   if (l2 == c1)
   {
-    int Prod[l1][c2];
 
+    int res[l1][c2];
     for (int i = 0; i < l1; i++)
     {
       for (int j = 0; j < c1; j++)
       {
-        scanf("%d", &M1[l1][c1]);
+        scanf("%d", &M1[i][j]);
       }
     }
 
@@ -31,37 +31,29 @@ int main(void)
     {
       for (int j = 0; j < c2; j++)
       {
-        scanf("%d", &M2[l2][c2]);
+        scanf("%d", &M2[i][j]);
       }
-    }
-
-    for (int i = 0; i < l1; i++)
-    {
-      while (k != l1)
-      {
-        for (int j = 0; j < c2; j++)
-        {
-          soma += (M1[i][j] * M2[j][k]);
-        }
-        Prod[i][k] = soma;
-        soma = 0;
-        k++;
-      }
-      k = 0;
     }
 
     for (int i = 0; i < l1; i++)
     {
       for (int j = 0; j < c2; j++)
       {
-        printf("%d ", Prod[i][j]);
+        res[i][j] = 0;
+        for (int k = 0; k < l1; k++)
+        {
+          res[i][j] += M1[i][k] * M2[k][j];
+        }
+      }
+    }
+    for (int i = 0; i < l1; i++)
+    {
+      for (int j = 0; j < c2; j++)
+      {
+        printf("%d ", res[i][j]);
       }
       printf("\n");
     }
-  }
-  else
-  {
-    printf("Não é possível fazer a multiplicação das duas matrizes!\n");
   }
 
   return 0;
